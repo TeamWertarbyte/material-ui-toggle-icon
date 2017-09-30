2a9e2e8c1181590d7a260aaa552bc97c64072563
leMaik <an@maik.tk>
2017-09-30 16:06:13 +0200

    Add fallback for browsers that don't support CSS clip-path (e.g. IE11, Edge).

--- src/ToggleIcon.js
+++ src/ToggleIcon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { clipPathSupported } from './util'
 
 const getStyles = (props) => ({
   root: {
@@ -9,8 +10,9 @@ const getStyles = (props) => ({
     display: 'inline-block'
   },
   offIcon: {
-    transition: '550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+    transition: 'clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
     clipPath: props.on ? 'polygon(0% 0%, 0% 0%, 0% 0%)' : 'polygon(0% 200%, 0% 0%, 200% 0%)',
+    visibility: !props.on || clipPathSupported() ? 'visible' : 'hidden',
     width: '100%',
     height: '100%',
     position: 'absolute',
@@ -18,8 +20,9 @@ const getStyles = (props) => ({
     top: 0
   },
   onIcon: {
-    transition: '550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
+    transition: 'clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
     clipPath: props.on ? 'polygon(100% -100%, 100% 100%, -100% 100%)' : 'polygon(100% 100%, 100% 100%, 100% 100%)',
+    visibility: props.on || clipPathSupported() ? 'visible' : 'hidden',
     width: '100%',
     height: '100%',
     position: 'absolute',
---
+++ src/util.js
@@ -0,0 +1,29 @@
+let _clipPathSupported = null
+
+// Check if clip-path is supported. From http://stackoverflow.com/a/30041538.
+export function clipPathSupported () {
+  if (_clipPathSupported != null) {
+    return _clipPathSupported
+  }
+
+  const base = 'clipPath'
+  const prefixes = ['webkit', 'moz', 'ms', 'o']
+  const properties = [base, ...prefixes.map((prefix) => prefix + base.charAt(0).toUpperCase() + base.slice(1))]
+  const testElement = document.createElement('testelement')
+  const attribute = 'polygon(50% 0%, 0% 100%, 100% 100%)'
+
+  // Interate over the properties and see if they pass two tests.
+  for (const property of properties) {
+    // First, they need to even support clip-path (IE <= 11 does not)...
+    if (testElement.style[property] === '') {
+      // Second, we need to see what happens when we try to create a CSS shape...
+      testElement.style[property] = attribute
+      if (testElement.style[property] !== '') {
+        _clipPathSupported = true
+        return true
+      }
+    }
+  }
+  _clipPathSupported = false
+  return false
+}
