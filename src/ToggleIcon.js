91cbeed919fa23997955a6e15f67fd0218bc59df
Maik Marschner <an@maik.tk>
2017-10-25 14:21:25 +0200

    Port to material-ui 1.0.0-beta.

--- src/ToggleIcon.js
+++ src/ToggleIcon.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withStyles } from 'material-ui/styles'
 import { clipPathSupported } from './util'
 
 const clipPath = (value) => ({
@@ -7,7 +8,7 @@ const clipPath = (value) => ({
   clipPath: value
 })
 
-const getStyles = (props) => ({
+const styles = {
   root: {
     width: 24,
     height: 24,
@@ -16,8 +17,6 @@ const getStyles = (props) => ({
   },
   offIcon: {
     transition: 'clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1), -webkit-clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
-    ...clipPath(props.on ? 'polygon(0% 0%, 0% 0%, 0% 0%)' : 'polygon(0% 200%, 0% 0%, 200% 0%)'),
-    visibility: !props.on || clipPathSupported() ? 'visible' : 'hidden',
     width: '100%',
     height: '100%',
     position: 'absolute',
@@ -26,57 +25,47 @@ const getStyles = (props) => ({
   },
   onIcon: {
     transition: 'clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1), -webkit-clip-path 550ms cubic-bezier(0.4, 0.0, 0.2, 1)',
-    ...clipPath(props.on ? 'polygon(100% -100%, 100% 100%, -100% 100%)' : 'polygon(100% 100%, 100% 100%, 100% 100%)'),
-    visibility: props.on || clipPathSupported() ? 'visible' : 'hidden',
     width: '100%',
     height: '100%',
     position: 'absolute',
     left: 0,
     top: 0
   }
-})
+}
 
 /**
  * An animated toggle icon.
  */
-export default function ToggleIcon (props) {
+function ToggleIcon (props) {
   const {
-    color,
-    hoverColor,
+    classes,
     offIcon,
     onIcon,
-    on, // eslint-disable-line
-    style,
+    on,
     ...other
   } = props
 
-  const styles = getStyles(props)
-
   return (
-    <div style={{ ...styles.root, ...style }} {...other}>
+    <div className={classes.root} {...other}>
       {React.cloneElement(offIcon, {
-        style: { ...style, ...styles.offIcon },
-        color,
-        hoverColor
+        className: classes.offIcon,
+        style: {
+          ...clipPath(on ? 'polygon(0% 0%, 0% 0%, 0% 0%)' : 'polygon(0% 200%, 0% 0%, 200% 0%)'),
+          visibility: !on || clipPathSupported() ? 'visible' : 'hidden'
+        }
       })}
       {React.cloneElement(onIcon, {
-        style: { ...style, ...styles.onIcon, ...style },
-        color,
-        hoverColor
+        className: classes.onIcon,
+        style: {
+          ...clipPath(on ? 'polygon(100% -100%, 100% 100%, -100% 100%)' : 'polygon(100% 100%, 100% 100%, 100% 100%)'),
+          visibility: on || clipPathSupported() ? 'visible' : 'hidden'
+        }
       })}
     </div>
   )
 }
 
 ToggleIcon.propTypes = {
-  /**
-   * This is the fill color of the svg icon. If not specified, this component will default to muiTheme.palette.textColor.
-   */
-  color: PropTypes.string,
-  /**
-   * This is the icon color when the mouse hovers over the icon.
-   */
-  hoverColor: PropTypes.string,
   /**
    * The icon to be displayed if the `on` prop is set to `false`.
    */
@@ -88,9 +77,7 @@ ToggleIcon.propTypes = {
   /**
    * Controls whether the `onIcon` or the `offIcon` is displayed.
    */
-  on: PropTypes.bool.isRequired,
-  /**
-   * Override the inline-styles of the root element.
-   */
-  style: PropTypes.object
+  on: PropTypes.bool.isRequired
 }
+
+export default withStyles(styles)(ToggleIcon)
